1627c19e9272155e8d4210379a760957cc0556ae
Philipp Grogg <grogg@puzzle.ch>
2015-05-31 22:10:27 +0200

    bugfix: not only instrument output of void returning methods

--- modjprof-agent/src/ch/puzzle/modjprof/InstrumentMeasurementPonitsMethodVisitor.java
+++ modjprof-agent/src/ch/puzzle/modjprof/InstrumentMeasurementPonitsMethodVisitor.java
@@ -2,9 +2,9 @@ package ch.puzzle.modjprof;
 
 import static org.objectweb.asm.Opcodes.ASM5;
 import static org.objectweb.asm.Opcodes.INVOKESTATIC;
-import static org.objectweb.asm.Opcodes.RETURN;
 
 import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 
 public class InstrumentMeasurementPonitsMethodVisitor extends MethodVisitor {
 
@@ -29,9 +29,18 @@ public class InstrumentMeasurementPonitsMethodVisitor extends MethodVisitor {
 
     @Override
     public void visitInsn(int opcode) {
-        if (opcode == RETURN) {
+        if (opcode >= Opcodes.IRETURN && opcode <= Opcodes.RETURN) {
             mv.visitMethodInsn(INVOKESTATIC, "ch/puzzle/modjprof/Profiler", "notifyExitMethod", "()V", false);
+            //            mv.visitMethodInsn(INVOKESTATIC, "ch/puzzle/modjprof/Profiler", "notifyExitMethod", "(Ljava/lang/String;)V", false);
         }
         super.visitInsn(opcode);
     }
+
+    //    @Override
+    //    public void visitEnd() {
+    //        mv.visitLdcInsn("L" + className + "; " + methodName + " " + methodDescriptor);
+    //        mv.visitMethodInsn(INVOKESTATIC, "ch/puzzle/modjprof/Profiler", "notifyExitMethod", "(Ljava/lang/String;)V", false);
+    //        super.visitEnd();
+    //    }
+
 }
--- modjprof-agent/src/ch/puzzle/modjprof/Profiler.java
+++ modjprof-agent/src/ch/puzzle/modjprof/Profiler.java
@@ -12,17 +12,24 @@ public class Profiler {
 
 	public static void premain(String agentArgs, Instrumentation inst)
 			throws IOException {
-		writer = new PrintWriter(new FileWriter(new File("/tmp/profiler.txt")),
+        writer = new PrintWriter(new FileWriter(new File("/tmp/profiler.trc")),
 				true);
-        System.out.println("Starting the ASM agent...");
 		inst.addTransformer(new ASMClassFileTransformer());
 	}
 
 	public static void notifyEnterMethod(String methodSignature) {
-		writer.println("> " + System.nanoTime() + " " + methodSignature);
+        writer.println("> " + getMilliString() + " " + methodSignature);
 	}
 
-	public static void notifyExitMethod() {
-		writer.println("< " + System.nanoTime());
+    public static void notifyExitMethod() {
+        writer.println("< " + getMilliString());
 	}
+
+    private static String getMilliString() {
+        long nanoTime = System.nanoTime();
+        int pos = 6;
+        String x = Long.toString(nanoTime);
+        x = x.substring(0, x.length() - pos) + "." + x.substring(x.length() - pos, x.length());
+        return x;
+    }
 }
