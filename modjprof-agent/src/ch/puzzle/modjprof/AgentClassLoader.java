1b81c9f572814d251d1a401324d35033ba2e6667
Philipp Grogg <philipp.grogg@gmail.com>
2015-06-11 00:24:25 +0200

    ClassLoader changed to URLClassLoader

--- modjprof-agent/src/ch/puzzle/modjprof/AgentClassLoader.java
+++ modjprof-agent/src/ch/puzzle/modjprof/AgentClassLoader.java
@@ -1,14 +1,27 @@
 package ch.puzzle.modjprof;
 
-public class AgentClassLoader extends ClassLoader {
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Enumeration;
+
+public class AgentClassLoader extends URLClassLoader {
 
     static {
         System.err
                 .println("*** AgentClassLoader loaded by " + AgentClassLoader.class.getClassLoader().getClass().getSimpleName());
     }
 
+    public AgentClassLoader() {
+        // set the parent class loader and add this jar to search path
+        super(new URL[] { AgentClassLoader.class.getProtectionDomain().getCodeSource().getLocation() }, AgentClassLoader.class
+                .getClassLoader());
+    }
+
     @Override
     protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+
+        //Always delegate JDK classes to parent.
         if (name.startsWith("java.") || name.startsWith("javax.")) {
             return super.loadClass(name, resolve);
         }
@@ -22,12 +35,12 @@ public class AgentClassLoader extends ClassLoader {
                 try {
                     c = findClass(name);
                 } catch (ClassNotFoundException cnfe) {
-                    // ignore
+                    // ClassNotFoundException thrown if class not found
+                    // in the search path of this class loader
                 }
             }
 
             // if we could not find it, delegate to parent
-            // Note that we don't attempt to catch any ClassNotFoundException
             if (c == null) {
                 if (getParent() != null) {
                     c = getParent().loadClass(name);
@@ -46,4 +59,29 @@ public class AgentClassLoader extends ClassLoader {
         }
     }
 
+    @Override
+    public URL getResource(String name) {
+        // Search local (child) resources
+        URL url = findResource(name);
+
+        if (url != null) {
+            return url;
+        }
+
+        // if we could not find it, delegate to parent
+        return getParent().getResource(name);
+    }
+
+    @Override
+    public Enumeration<URL> getResources(String name) throws IOException {
+        // Search local (child) resources
+        Enumeration<URL> urls = findResources(name);
+
+        if (urls != null && urls.hasMoreElements()) {
+            return urls;
+        }
+
+        // if we could not find it, delegate to parent
+        return getParent().getResources(name);
+    }
 }
