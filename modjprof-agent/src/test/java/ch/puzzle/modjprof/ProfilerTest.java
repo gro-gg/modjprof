188fd6ac70e1dd55c7e825173470f148be56e05e
Philipp Grogg <grogg@puzzle.ch>
2015-06-17 21:53:12 +0200

    first dummy unit test implemented; project directories refactored

---
+++ modjprof-agent/src/test/java/ch/puzzle/modjprof/ProfilerTest.java
@@ -0,0 +1,13 @@
+package ch.puzzle.modjprof;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ProfilerTest {
+
+    @Test
+    public void shouldSuccess() {
+        assertTrue(true);
+    }
+}
