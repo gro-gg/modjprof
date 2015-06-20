ade9d5eee4fc3e761c6ca5b4d552b892d288fda9
Philipp Grogg <philipp.grogg@gmail.com>
2015-06-20 16:18:50 +0200

    new source folder for integration tests

--- modjprof-agent/src/integration-test/java/ch/puzzle/modjprof/instrumentation/InstrumentationIT.java
+++ modjprof-agent/src/integration-test/java/ch/puzzle/modjprof/instrumentation/InstrumentationIT.java
@@ -1,15 +1,14 @@
-package ch.puzzle.modjprof;
+package ch.puzzle.modjprof.instrumentation;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.ProcessBuilder.Redirect;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import junit.framework.TestCase;
 
-public class MainIT extends TestCase {
+public class InstrumentationIT extends TestCase {
 
     private static final String AGENT_JAR = "target/modjprodf-agent-0.0.1-SNAPSHOT-jar-with-dependencies.jar";
     private static final String APPLICATION_JAR = "target/sample-application-0.0.1-SNAPSHOT.jar";
@@ -18,17 +17,17 @@ public class MainIT extends TestCase {
         assertEquals(0, execute(new String[] {}));
     }
 
-    private int execute(String[] args) throws Exception {
-        ProcessBuilder pb = new ProcessBuilder(buildCommandList(args));
-        pb.redirectOutput(Redirect.INHERIT);
-        pb.redirectError(Redirect.INHERIT);
+    private int execute(String[] args) throws IOException, InterruptedException {
+        ProcessBuilder pb = new ProcessBuilder(buildCommandLine(args));
+        // pb.redirectOutput(Redirect.INHERIT);
+        // pb.redirectError(Redirect.INHERIT);
 
         Process p = pb.start();
         p.waitFor();
         return p.exitValue();
     }
 
-    private List<String> buildCommandList(String[] args) throws IOException {
+    private List<String> buildCommandLine(String[] args) throws IOException {
         File jar = new File(APPLICATION_JAR);
         List<String> command = new ArrayList<>();
         command.add("java");
