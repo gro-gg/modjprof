63d5a724fc03ab1260b2869fcffd9df4dfdb17d0
Philipp Grogg <philipp.grogg@gmail.com>
2015-06-21 14:47:19 +0200

    file comparison implemented

--- modjprof-agent/src/integration-test/java/ch/puzzle/modjprof/instrumentation/InstrumentationIT.java
+++ modjprof-agent/src/integration-test/java/ch/puzzle/modjprof/instrumentation/InstrumentationIT.java
@@ -1,5 +1,9 @@
 package ch.puzzle.modjprof.instrumentation;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 import java.io.File;
 import java.io.IOException;
 import java.lang.ProcessBuilder.Redirect;
@@ -7,16 +11,38 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import junit.framework.TestCase;
+import org.apache.commons.io.Charsets;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
 
-public class InstrumentationIT extends TestCase {
+public class InstrumentationIT {
 
     private static final String VERSION = "0.0.1-SNAPSHOT";
     private static final String AGENT_JAR = "target/modjprodf-agent-" + VERSION + "-jar-with-dependencies.jar";
     private static final String APPLICATION_JAR = "../sample-application/target/sample-application-" + VERSION + ".jar";
 
-    public void testExecute() throws Exception {
-        assertEquals(0, execute(new String[] {}));
+    @Test
+    public void testExecute() throws IOException {
+        try {
+            assertThat(execute(new String[] {}), is(equalTo(0)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.err.println("application terminated");
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("ch/puzzle/modjprof/instrumentation/profiler.trc").getFile());
+        List<String> expectedLines = FileUtils.readLines(file);
+        List<String> resultLines = FileUtils.readLines(new File("/tmp/profiler.trc"), Charsets.UTF_8);
+        System.err.println(expectedLines.size());
+        assertThat(expectedLines.size(), is(equalTo(resultLines.size())));
+        for (int i = 0; i < expectedLines.size(); i++) {
+            System.err.println(resultLines.get(i));
+            // TODO ignore timestamp
+            assertThat(resultLines.get(i), is(expectedLines.get(i)));
+        }
+
     }
 
     private int execute(String[] args) throws IOException, InterruptedException {
@@ -25,8 +51,8 @@ public class InstrumentationIT extends TestCase {
         pb.redirectError(Redirect.INHERIT);
 
         Process p = pb.start();
-        p.waitFor();
-        return p.exitValue();
+        return p.waitFor();
+        //        return p.exitValue();
     }
 
     private List<String> buildCommandLine(String[] args) throws IOException {
@@ -39,5 +65,5 @@ public class InstrumentationIT extends TestCase {
         command.addAll(Arrays.asList(args));
         return command;
     }
-
 }
+
