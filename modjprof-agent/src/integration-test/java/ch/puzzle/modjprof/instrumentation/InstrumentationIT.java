1c8426c961b5be53d84ac5d368ec599b300da471
Philipp Grogg <philipp.grogg@gmail.com>
2015-06-22 10:04:33 +0200

    ignoring the timestamp; test refactored

--- modjprof-agent/src/integration-test/java/ch/puzzle/modjprof/instrumentation/InstrumentationIT.java
+++ modjprof-agent/src/integration-test/java/ch/puzzle/modjprof/instrumentation/InstrumentationIT.java
@@ -17,32 +17,24 @@ import org.junit.Test;
 
 public class InstrumentationIT {
 
+    private static final String RESULT_TRC_FILE = "/tmp/profiler.trc";
+    private static final String EXPECTED_TRC_FILE = "ch/puzzle/modjprof/instrumentation/profiler.trc";
     private static final String VERSION = "0.0.1-SNAPSHOT";
     private static final String AGENT_JAR = "target/modjprodf-agent-" + VERSION + "-jar-with-dependencies.jar";
     private static final String APPLICATION_JAR = "../sample-application/target/sample-application-" + VERSION + ".jar";
 
     @Test
-    public void testExecute() throws IOException {
-        try {
-            assertThat(execute(new String[] {}), is(equalTo(0)));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void testExecute() throws IOException, InterruptedException {
+        assertThat(execute(new String[] {}), is(equalTo(0)));
 
-        System.err.println("application terminated");
-
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("ch/puzzle/modjprof/instrumentation/profiler.trc").getFile());
+        File file = new File(getClass().getClassLoader().getResource(EXPECTED_TRC_FILE).getFile());
         List<String> expectedLines = FileUtils.readLines(file);
-        List<String> resultLines = FileUtils.readLines(new File("/tmp/profiler.trc"), Charsets.UTF_8);
-        System.err.println(expectedLines.size());
+        List<String> resultLines = FileUtils.readLines(new File(RESULT_TRC_FILE), Charsets.UTF_8);
+
         assertThat(expectedLines.size(), is(equalTo(resultLines.size())));
         for (int i = 0; i < expectedLines.size(); i++) {
-            System.err.println(resultLines.get(i));
-            // TODO ignore timestamp
-            assertThat(resultLines.get(i), is(expectedLines.get(i)));
+            assertThat(removeTimestamp(resultLines.get(i)), is(removeTimestamp(expectedLines.get(i))));
         }
-
     }
 
     private int execute(String[] args) throws IOException, InterruptedException {
@@ -52,7 +44,6 @@ public class InstrumentationIT {
 
         Process p = pb.start();
         return p.waitFor();
-        //        return p.exitValue();
     }
 
     private List<String> buildCommandLine(String[] args) throws IOException {
@@ -64,6 +55,17 @@ public class InstrumentationIT {
         command.add(jar.getCanonicalPath());
         command.addAll(Arrays.asList(args));
         return command;
+    }
+
+    private String removeTimestamp(String line) {
+        String[] split = line.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < split.length; i++) {
+            if (i != 1) {
+                sb.append(split[i]);
+            }
+        }
+        return sb.toString();
     }
 }
 
