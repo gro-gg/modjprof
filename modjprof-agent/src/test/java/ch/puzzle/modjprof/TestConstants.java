fd2815822d39ce9cd1ac51bca97be19321d5a5e2
Philipp Grogg <philipp.grogg@gmail.com>
2015-07-04 14:35:53 +0200

    integration test for class loading added

---
+++ modjprof-agent/src/test/java/ch/puzzle/modjprof/TestConstants.java
@@ -0,0 +1,15 @@
+package ch.puzzle.modjprof;
+
+public class TestConstants {
+
+    private static final String VERSION = "0.0.1-SNAPSHOT";
+
+    public static final String AGENT_JAR = "modjprodf-agent-" + VERSION + "-jar-with-dependencies.jar";
+
+    public static final String APPLICATION_JAR = "sample-application-" + VERSION + ".jar";
+
+    public static final String AGENT_JAR_LOCATION = "target/" + AGENT_JAR;
+
+    public static final String APPLICATION_JAR_LOCATION = "../sample-application/target/" + APPLICATION_JAR;
+
+}
