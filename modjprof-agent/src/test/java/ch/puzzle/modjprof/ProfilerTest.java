2d97572918bc988f000f8cfbff85a8493063780b
Philipp Grogg <grogg@puzzle.ch>
2015-07-23 16:20:40 +0200

    license / copyright header added

--- modjprof-agent/src/test/java/ch/puzzle/modjprof/ProfilerTest.java
+++ modjprof-agent/src/test/java/ch/puzzle/modjprof/ProfilerTest.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright 2015 The modjprof Project Developers. See the COPYRIGHT file at the top-level directory of this distribution and at
+ * https://github.com/gro-gg/modjprof/blob/master/COPYRIGHT.
+ *
+ * This file is part of modjprof Project. It is subject to the license terms in the LICENSE file found in the top-level directory
+ * of this distribution and at https://github.com/gro-gg/modjprof/blob/master/LICENSE. No part of modjprof Project, including this
+ * file, may be copied, modified, propagated, or distributed except according to the terms contained in the LICENSE file.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See LICENSE file for more details.
+ */
 package ch.puzzle.modjprof;
 
 import static org.junit.Assert.assertTrue;
