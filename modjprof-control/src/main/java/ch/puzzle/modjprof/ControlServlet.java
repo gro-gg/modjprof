78f7bc944579119595e3b26bcace3cf80ca89d38
Philipp Grogg <philipp.grogg@gmail.com>
2015-08-06 18:30:01 +0200

    first version of control servlet

---
+++ modjprof-control/src/main/java/ch/puzzle/modjprof/ControlServlet.java
@@ -0,0 +1,51 @@
+package ch.puzzle.modjprof;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ControlServlet", urlPatterns = { "/" })
+public class ControlServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 7289197726261711665L;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>modjprof ControlServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>modjprof ControlServlet</h1>");
+            out.println("<h1>ContextPath: " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
