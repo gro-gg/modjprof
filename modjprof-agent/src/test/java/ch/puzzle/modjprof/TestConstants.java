77f2d1e490ef717adfa870fed6541a977b9dde8f
Philipp Grogg <philipp.grogg@gmail.com>
2015-08-07 09:15:07 +0200

    names of target jar/war changed

--- modjprof-agent/src/test/java/ch/puzzle/modjprof/TestConstants.java
+++ modjprof-agent/src/test/java/ch/puzzle/modjprof/TestConstants.java
@@ -13,11 +13,9 @@ package ch.puzzle.modjprof;
 
 public class TestConstants {
 
-    private static final String VERSION = "0.0.1-SNAPSHOT";
+    public static final String AGENT_JAR = "modjprof-agent.jar";
 
-    public static final String AGENT_JAR = "modjprof-agent-" + VERSION + ".jar";
-
-    public static final String APPLICATION_JAR = "sample-application-" + VERSION + "-jar-with-dependencies.jar";
+    public static final String APPLICATION_JAR = "sample-application-jar-with-dependencies.jar";
 
     public static final String AGENT_JAR_LOCATION = "target/" + AGENT_JAR;
 
