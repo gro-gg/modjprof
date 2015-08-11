469c12b81b0574aa01cae0e23bd9645375f33130
Philipp Grogg <philipp.grogg@gmail.com>
2015-08-11 22:17:55 +0200

    sample-servlet added

---
+++ sample-servlet/src/main/java/ch/puzzle/sample/SampleServlet.java
@@ -0,0 +1,51 @@
+package ch.puzzle.sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "ControlServlet", urlPatterns = { "/*" })
+public class SampleServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 7289197726261711665L;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>SampleServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>SampleServlet</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
