1573c571c421be26fde58c89bcc0f9733030fb9f
Philipp Grogg <philipp.grogg@gmail.com>
2015-08-23 21:11:38 +0200

    apache license added to request-filter subproject

--- modjprof-filter/src/main/java/ch/puzzle/modjprof/filter/ModjprofServletFilter.java
+++ modjprof-filter/src/main/java/ch/puzzle/modjprof/filter/ModjprofServletFilter.java
@@ -1,3 +1,26 @@
+/**
+ * Copyright 2015 The modjprof Project Developers. See the COPYRIGHT file at the top-level directory of this distribution and at
+ * https://github.com/gro-gg/modjprof/blob/master/COPYRIGHT.
+ *
+ * This file is part of modjprof Project. It is subject to the license terms in the LICENSE file found in the top-level directory
+ * of this distribution and at https://github.com/gro-gg/modjprof/blob/master/LICENSE. No part of modjprof Project, including this
+ * file, may be copied, modified, propagated, or distributed except according to the terms contained in the LICENSE file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See LICENSE file for more details.
+ */
 package ch.puzzle.modjprof.filter;
 
 import java.io.IOException;
